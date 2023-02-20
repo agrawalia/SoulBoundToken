67f93a0a9390c0fa69df8b23587256f75742c59b
Ishan Agrawal <ishan@Ishans-MacBook-Pro.local>
2023-02-20 23:53:48 +0530

    Added comment

--- scripts/mintSBT.js
+++ scripts/mintSBT.js
@@ -1,6 +1,6 @@
 async function main() {
     const to = "0x3511a6FfBc713E53339BE052795e095c2E0ce30D";
-    const uri = "QmZ3yPetFfB7kmDeFVqXppxGH6TzFM2PWJjZvTjxWhZWP3";
+    const uri = "QmZ3yPetFfB7kmDeFVqXppxGH6TzFM2PWJjZvTjxWhZWP3"; // CID of .json metadata file from pinata
     const address = "0xBc41DE01174589FfdFCFFbB66Cb65e8Ed7d4963D";
     const SoulBoundTest = await hre.ethers.getContractFactory("SoulBoundTest");
     const soulBoundTest = await SoulBoundTest.attach(address);
